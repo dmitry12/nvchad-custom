215d7aa57f0b62a73c21a14e8e6c052f1f1349f2
Dmitry Astapenko <dmitry.astapenko@gmail.com>
2021-10-22 13:43:52 +0200

    add eslint support

---
+++ chadrc.lua
@@ -0,0 +1,30 @@
+local M = {}
+M.options, M.ui, M.mappings, M.plugins = {}, {}, {}, {}
+
+-- To use this file, copy the structure of `core/default_config.lua`,
+-- examples of setting relative number & changing theme:
+
+-- M.options = {
+--    relativenumber = true,
+-- }
+
+-- M.ui = {
+--   theme = "nord"
+-- }
+
+M.plugins = {
+   options = {
+      lspconfig = {
+      setup_lspconf = "custom.plugins.lspconfig",
+      },
+   },
+   -- To change the Packer `config` of a plugin that comes with NvChad,
+   -- add a table entry below matching the plugin github name
+   --              '-' -> '_', remove any '.lua', '.nvim' extensions
+   -- this string will be called in a `require`
+   --              use "(custom.configs).my_func()" to call a function
+   --              use "custom.blankline" to call a file
+   default_plugin_config_replace = {},
+}
+
+return M
--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -1,34 +1,88 @@
 local M = {}
 
 M.setup_lsp = function(attach, capabilities)
-   local lspconfig = require "lspconfig"
+  local lspconfig = require "lspconfig"
 
-   -- lspservers with default config
+  -- lspservers with default config
 
-   -- local servers = { "html", "cssls", "pyright" }
-   local servers = {}
+  -- local servers = { "html", "cssls", "pyright" }
+  local servers = {}
 
-   for _, lsp in ipairs(servers) do
-      lspconfig[lsp].setup {
-         on_attach = attach,
-         capabilities = capabilities,
-         flags = {
-            debounce_text_changes = 150,
-         },
-      }
-   end
-   
-   -- typescript
+  for _, lsp in ipairs(servers) do
+    lspconfig[lsp].setup {
+      on_attach = attach,
+      capabilities = capabilities,
+      flags = {
+        debounce_text_changes = 150,
+      },
+    }
+  end
 
- lspconfig.tsserver.setup {
-      on_attach = function(client, bufnr)
-         client.resolved_capabilities.document_formatting = false
-         vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", {})
-      end,
-   }
+  -- typescript
 
--- the above tsserver config will remvoe the tsserver's inbuilt formatting 
--- since I use null-ls with denofmt for formatting ts/js stuff.
+  lspconfig.tsserver.setup {
+    on_attach = function(client, bufnr)
+      client.resolved_capabilities.document_formatting = false
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", {})
+    end,
+  }
+
+  -- from: https://github.com/marwan38/NvChad/blob/main/lua/plugins/configs/lspconfig.lua#L109
+
+  local diagnosticls_filetypes = {
+    typescript = "eslint",
+    typescriptreact = "eslint",
+    php = "phpcs",
+  }
+
+  local diagnosticls_linters = {
+    eslint = {
+      sourceName = "eslint",
+      command = "eslint_d",
+      rootPatterns = { ".eslintrc.js", ".eslintrc.json", "package.json" },
+      debounce = 100,
+      args = { "--stdin", "--stdin-filename", "%filepath", "--format", "json" },
+      parseJson = {
+        errorsRoot = "[0].messages",
+        line = "line",
+        column = "column",
+        endLine = "endLine",
+        endColumn = "endColumn",
+        message = "${message} [${ruleId}]",
+        security = "severity",
+      },
+      securities = { [2] = "error", [1] = "warning" },
+    },
+    phpcs = {
+      sourceName = "phpcs",
+      command = "./vendor/bin/phpcs",
+      debounce = 100,
+      args = { "--report=emacs", "-s", "-" },
+      offsetLine = 0,
+      offsetColumn = 0,
+      formatLines = 1,
+      formatPattern = {
+        [[^.*:(\d+):(\d+):\s+(.*)\s+-\s+(.*)(\r|\n)*$]],
+        { line = 1, column = 2, security = 3, message = { "[phpcs] ", 4 } },
+      },
+      securities = { error = "error", warning = "warning" },
+      rootPatterns = { ".git", "vendor", "composer.json" },
+    },
+  }
+
+  lspconfig.diagnosticls.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    flags = {
+      debounce_text_changes = 500,
+    },
+    filetypes = vim.tbl_keys(diagnosticls_filetypes),
+    init_options = {
+      filetypes = diagnosticls_filetypes,
+      linters = diagnosticls_linters,
+    },
+  }
+
 end
 
 return M
